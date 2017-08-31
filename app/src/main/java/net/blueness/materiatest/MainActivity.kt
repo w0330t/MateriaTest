1ff1cad9ce0a6187c11804bd5fa3fd5c2b9a7fd1
Blueness <w0330t@gmail.com>
2017-08-31 18:38:02 +0800

    Build DrawerLayout

--- app/src/main/java/net/blueness/materiatest/MainActivity.kt
+++ app/src/main/java/net/blueness/materiatest/MainActivity.kt
@@ -2,6 +2,8 @@ package net.blueness.materiatest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
@@ -10,13 +12,23 @@ import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
+    var mDrawerLayout: DrawerLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = find(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        mDrawerLayout = find(R.id.drawer_layout)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar, menu)
@@ -25,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
+            android.R.id.home -> mDrawerLayout?.openDrawer(GravityCompat.START)
             R.id.backup -> toast("You clicked Backup")
             R.id.delete -> toast("You clicked Delete")
             R.id.setting -> toast("You clicked Setting")
